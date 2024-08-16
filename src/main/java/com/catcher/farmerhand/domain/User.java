c69deb90fd983a68bef1bde55ce166ef3b525ac7
mingjuu <tisu79@naver.com>
2024-08-17 00:32:28 +0900

    feat : USER entity 작성

---
+++ src/main/java/com/catcher/farmerhand/domain/User.java
@@ -0,0 +1,43 @@
+package com.catcher.farmerhand.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        super();
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
