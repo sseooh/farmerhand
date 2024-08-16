1347c13d9c223d294c007d2911b4b7fb940d02fb
sseooh <sseooh@gmail.com>
2024-08-16 23:53:32 +0900

    Initial commit

---
+++ src/main/java/com/catcher/farmerhand/FarmerhandApplication.java
@@ -0,0 +1,13 @@
+package com.catcher.farmerhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerhandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catcher/farmerhand/FarmerhandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catcher.farmerhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
